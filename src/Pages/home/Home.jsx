efc21ef9bd3a73b20bb79b2251da267032fb027b
reynaldoabv <reynaldob.webdev@gmail.com>
2020-03-11 16:35:20 -0300

    Created project and added files for homepage

---
+++ src/Pages/home/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './home.scss'
+import PropTypes from 'prop-types'
+
+const Home = props => {
+    return (
+        <div className="container home-page">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKQUETS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SNEAKERS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WOMEN</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">MEN</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+Home.propTypes = {
+
+}
+
+export default Home;
