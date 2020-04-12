1c54adc2e066fbb82b7a3e40ef382c800cb0cf76
reynaldoabv <reynaldob.webdev@gmail.com>
2020-04-12 18:36:01 -0400

    Removing google maps api key

--- src/components/google-maps/GoogleMaps.jsx
+++ src/components/google-maps/GoogleMaps.jsx
@@ -22,7 +22,7 @@ class GoogleMaps extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyB7TrPWDn7O_URpoL7pX-tzg9uv_6kdlYI' }}
+          bootstrapURLKeys={{ key: 'Google Maps Api key here' }}
           defaultCenter={center}
           defaultZoom={zoom}
           marker={marker}
