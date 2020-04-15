978b2b6e0963a911dbac8abc18edff685c11161f
reynaldoabv <reynaldob.webdev@gmail.com>
2020-04-15 12:31:42 -0400

    Fixing link tag in collectionPreview component

--- client/src/components/collection-preview/CollectionPreview.jsx
+++ client/src/components/collection-preview/CollectionPreview.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import './collection-preview.scss';
 
 import CollectionItem from '../collection-item/CollectionItem';
+import { Link } from 'react-router-dom';
 
-const CollectionPreview = ({ title, items }) => (
-    <div className="collection-preview mt-5">
-        <h1 className="title"> { title.toUpperCase() } </h1>
-        <div className="preview">
-            {
-                items
-                .filter((item, idx) => idx < 4)
-                .map( item => (
-                    <CollectionItem key={item.id} item={item} />
-                ))
-            }
+const CollectionPreview = ({ title, items }) => {
+    console.log(title);
+    return (  
+
+        <div className="collection-preview mt-5">
+            <Link to={`/shop/${title.toLowerCase()}`} className="title" > { title.toUpperCase() } </Link>
+            <div className="preview">
+                {
+                    items
+                    .filter((item, idx) => idx < 4)
+                    .map( item => (
+                        <CollectionItem key={item.id} item={item} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 
 export default CollectionPreview;
